44f73ae6de8aafc72791e0b990ed443135428e14
Apoh Eldrige <peldrige8@gmail.com>
2020-07-29 23:34:35 +0100

    added styles

--- public/script.js
+++ public/script.js
@@ -9,22 +9,38 @@ searchBox.addListener("places_changed", () => {
   const place = searchBox.getPlaces()[0]; // select first place
   if (place == null) return; // make sure it is null
   const latitude = place.geometry.location.lat(); // similar to nav.geo in plain JS
-  const longitude = place.geometry.location.lon();
+  const longitude = place.geometry.location.lng();
 
   // using this info, our data will be posted to our server, and fetch will actually fetch that data
   fetch("/weather", {
-    method: "POST",
+    method: "POST", // to match the post we setup in our server.js express
     headers: {
-      "Content-Type": "application/json",
-      Accept: "application.json",
+      "Content-Type": "application/json", // we our sending json to the server
+      Accept: "application.json", // we will be retrieving json form the server
     },
     body: JSON.stringify({
       latitude: latitude,
       longitude: longitude,
-    }),
+    }), // send this to our server
+    // all of the above data is posted to our sever
   })
-    .then((response) => response.json())
+    .then((response) => response.json()) // convert our data to json
     .then((data) => {
-      setWeatherData(data, place.formatted_address);
-    });
+      setWeatherData(data, place.formatted_address); //from the places api
+      console.log(data);
+    })
+    .catch((err) => console.log(`an error occured: ${err}`));
 });
+
+const locationElt = document.querySelector("[data-location]");
+const statusElt = document.querySelector("[data-status]");
+const temperatureElt = document.querySelector("[data-temperature]");
+const precipitationElt = document.querySelector("[data-precipitation]");
+const windElt = document.querySelector("[data-wind]");
+
+function setWeatherData(data, place) {
+  locationElt.textContent = place;
+  statusElt.textContent = data.weather[0].description;
+  temperatureElt.textContent = data.main.temp;
+  precipitationElt.textContent = data.main.humidity;
+}
--- server.js
+++ server.js
@@ -3,10 +3,12 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-const API_kEY = process.env.OPENWEATHER_API_KEY;
+const API_KEY = process.env.OPENWEATHER_API_KEY;
 // populate the api_key var, with your api key details
 
 const express = require("express");
+const axios = require("axios");
+// axios works similarly to fetch
 
 const app = express();
 
@@ -17,9 +19,54 @@ app.use(express.static("public"));
 // that is our html will be served from here
 
 app.post("/weather", (req, res) => {
-  console.log(req.body);
+  const url = ` https://api.openweathermap.org/data/2.5/weather?lat=${req.body.latitude}&lon=${req.body.longitude}&appid=${API_KEY}`;
+  axios({
+    url : url,
+    responseType: 'json'
+  }).then(data => res.json(data)) // this will send a json version of our data, back to the script.js
+  // console.log(req.body);
 });
 
 app.listen(3000, () => {
   console.log("server started");
 });
+
+// {"coord": { "lon": 139,"lat": 35},
+//   "weather": [
+//     {
+//       "id": 800,
+//       "main": "Clear",
+//       "description": "clear sky",
+//       "icon": "01n"
+//     }
+//   ],
+//   "base": "stations",
+//   "main": {
+//     "temp": 281.52,
+//     "feels_like": 278.99,
+//     "temp_min": 280.15,
+//     "temp_max": 283.71,
+//     "pressure": 1016,
+//     "humidity": 93
+//   },
+//   "wind": {
+//     "speed": 0.47,
+//     "deg": 107.538
+//   },
+//   "clouds": {
+//     "all": 2
+//   },
+//   "dt": 1560350192,
+//   "sys": {
+//     "type": 3,
+//     "id": 2019346,
+//     "message": 0.0065,
+//     "country": "JP",
+//     "sunrise": 1560281377,
+//     "sunset": 1560333478
+//   },
+//   "timezone": 32400,
+//   "id": 1851632,
+//   "name": "Shuzenji",
+//   "cod": 200
+// }
